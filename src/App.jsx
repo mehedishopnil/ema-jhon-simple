a19a42b9feb0cb099ef49b2b6f7b35ac833526dc
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-04-27 08:42:42 +0600

    product load

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './compononets/Header/Header'
+import Shop from './compononets/Shop/Shop'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,7 @@ function App() {
   return (
   <div className='App'>
     <Header></Header>
+    <Shop></Shop>
   </div>
   )
 }
