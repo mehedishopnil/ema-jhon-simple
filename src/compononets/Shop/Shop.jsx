351bce466704fa3378ba290ea9c8a539adaf053f
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-04-28 11:53:17 +0600

    save cart to local storage

--- src/compononets/Shop/Shop.jsx
+++ src/compononets/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -13,10 +14,16 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    }, [])
+
     const handleAddToCart = (product) => {
         console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
