8e208947304df7f1878f3c6daa5268d75f26dc30
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-06-13 12:37:37 +0600

    log in form design complete

--- src/compononets/Login/Login.jsx
+++ src/compononets/Login/Login.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import './Login.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Login = () => {
     return (
-        <div>
-            <h3>Login part</h3>
+        <div className='form-box mt-8'>
+            <div className='form-container flex justify-center'>
+                <div>
+                    <h2 className='Login-title'>LogIn</h2>
+                </div>
+                <div>
+                    <form >
+                        <div>
+                            <label className='' htmlFor="email">Email:</label> <br />
+                            <input
+                                className='input-filed '
+                                type="email"
+                                id="email"
+                                name='email'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password:</label>
+                            <br />
+                            <input
+                                className='input-filed'
+                                type="password"
+                                id="password"
+                                name='password'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <button className='submit' type="submit">Login</button>
+                        </div>
+
+                        <div>
+
+                            <button className='google-submit' type="submit"> <span><img className='' src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" alt="" /></span>
+                                <span className='google-btn-text'>Continue with Google</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div></div>
+            </div>
         </div>
     );
 };
