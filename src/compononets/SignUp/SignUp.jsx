c51d40d1b2d073d568f711610802fe3435331df8
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-06-13 13:13:39 +0600

    sign up form done

--- src/compononets/SignUp/SignUp.jsx
+++ src/compononets/SignUp/SignUp.jsx
@@ -3,8 +3,53 @@ import './SignUp.css'
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>Please SignUp</h2>
+        <div className='form-box mt-8'>
+            <div className='form-container flex justify-center'>
+                <div>
+                    <h2 className='Login-title'>SignUp</h2>
+                </div>
+                <div>
+                    <form >
+                        <div>
+                            <label className='' htmlFor="email">Email:</label> <br />
+                            <input
+                                className='input-filed '
+                                type="email"
+                                id="email"
+                                name='email'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password:</label>
+                            <br />
+                            <input
+                                className='input-filed'
+                                type="password"
+                                id="password"
+                                name='password'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Confirm Password:</label>
+                            <br />
+                            <input
+                                className='input-filed'
+                                type="password"
+                                id="password"
+                                name='confirm'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <button className='submit' type="submit">Login</button>
+                        </div>
+
+                    </form>
+                </div>
+                <div></div>
+            </div>
         </div>
     );
 };
