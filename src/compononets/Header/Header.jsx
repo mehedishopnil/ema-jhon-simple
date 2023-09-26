4c26168c3191563e1f55c5fc21d7e8cdb95d9df9
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-09-26 13:03:55 +0600

    update pagination

--- src/compononets/Header/Header.jsx
+++ src/compononets/Header/Header.jsx
@@ -1,38 +1,61 @@
-import React, { useContext } from 'react';
-import './Header.css'
-import logo from '../../images/Logo.svg'
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../providers/AuthProvider';
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+import { useContext } from "react";
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
-    const {user,logOut,signIn } = useContext(AuthContext)
+  const { user, logOut } = useContext(AuthContext);
 
-    const handleSignOut = () =>{
-        logOut()
-        .then(() =>{
-            console.log('Log Out');
-        })
-        .catch(error =>{
-            console.log(error);
-        })
-    }
-  
+  const handleSignOut = () => {
+    logOut()
+      .then(() => {
+        console.log("Log Out");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div>
-                <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/sign-up">Sign Up</Link>
-                {user && <span>Welcome! <span>{user.email} <button onClick={handleSignOut} className='sign-out'>Sign Out</button></span></span>}
-                
-            </div>
-            
-        </nav>
-    );
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <Link to="/">Shop</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Inventory</Link>
+      </div>
+
+
+      <div>
+        {/* Check if 'user' is defined */}
+        {user ? (
+          // If 'user' is defined, show the 'FaUser' icon and 'Sign Out' button
+          <span className="user">
+            <FaUser></FaUser>
+          </span>
+        ) : (
+          // If 'user' is not defined, show the 'Login' button
+          <div>
+            <Link className="login-btn" to="/login"><button>Login</button></Link>
+          <Link to="/sign-up"><button>Sign Up</button></Link>
+          </div>
+        )}
+
+        {/* Always show the 'Sign Out' button (conditionally rendered) */}
+        {user && (
+          <span>
+            <button onClick={handleSignOut} className="sign-out text-white">
+              Sign Out
+            </button>
+          </span>
+        )}
+      </div>
+
+
+    </nav>
+  );
 };
 
 export default Header;
--- src/compononets/Shop/Shop.jsx
+++ src/compononets/Shop/Shop.jsx
@@ -1,110 +1,144 @@
-import React, { useEffect, useState } from 'react';
-import './Shop.css';
-import Product from '../Product/Product';
-import Cart from '../Cart/cart';
-import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fakedb';
-import { Link, useLoaderData } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from "react";
+import "./Shop.css";
+import Product from "../Product/Product";
+import Cart from "../Cart/cart";
+import {
+  addToDb,
+  deleteShoppingCart,
+  getShoppingCart,
+} from "../../utilities/fakedb";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    
-    // Done: 1. Determine the total number of items::
-    const {totalProducts} = useLoaderData()
-    console.log(totalProducts);
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    // 2.ToDo: Decide the number of items per page::
-    const itemsPerPage = 10;
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const saveCart = [];
 
-    // 3.Done: Total Pages::
-    const totalPages = Math.ceil(totalProducts / itemsPerPage);
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product._id === id);
 
-    // Page Numbers::
-    const pageNumbers = [...Array(totalPages).keys()]
-
-
-    useEffect(() => {
-        fetch('http://localhost:5000/products')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, [])
-
-    useEffect(() => {
-        const storedCart = getShoppingCart();
-        const saveCart = [];
-        //step-1: get id
-        for (const id in storedCart) {
-
-            //step-2: get the product by using id
-            const addedProduct = products.find(product => product._id === id);
-
-            //step-3: get the product quantity
-
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-
-                //step - 4: add the addedproduct to savedCart
-                saveCart.push(addedProduct);
-            }
-
-        }
-        //step -5: Set to cart
-        setCart(saveCart);
-    }, [products]);
-
-    const handleAddToCart = (product) => {
-        console.log(product);
-        const newCart = [...cart, product];
-        setCart(newCart);
-        addToDb(product._id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        saveCart.push(addedProduct);
+      }
     }
 
-    const handleClearCart = () => {
-        setCart([]);
-        deleteShoppingCart();
-    }
+    setCart(saveCart);
+  }, [products]);
 
-    return (
-        <>
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    products.map(product =>
-                        <Product key={product._id}
-                            product={product}
-                            handleAddToCart={handleAddToCart}
-                        >
-                        </Product>)
-                }
-            </div>
+  // Pagination data
+  const totalProducts = products.length;
+  const totalPages = Math.ceil(totalProducts / itemsPerPage);
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-            <div className="cart-container">
-                <Cart
-                    cart={cart}
-                    handleClearCart={handleClearCart}
-                >
-                    <Link className='review-order' to="/orders">
-                        <button className='cart-delete-btn checkout'>
-                        Review Order
-                            <FontAwesomeIcon className='cart-delete-icon' icon={faArrowRight} />
-                        </button>
-                    </Link>
-                </Cart>
-            </div>
+  // Handle Add to Cart
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product._id);
+  };
+
+  // Handle Clear Cart
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
+
+  // Handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Current Page Data
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentData = products.slice(startIndex, endIndex);
+
+  // Options for items per page select
+  const options = [5, 10, 20];
+
+
+  return (
+    <>
+      <div className="shop-container">
+        <div className="products-container">
+          {currentData.map((product) => (
+            <Product
+              key={product._id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Product>
+          ))}
         </div>
 
-        {/* Pagination */}
-        <div className='pagination'>
-                {
-                    pageNumbers.map(number => <button key={number}>{number}</button>)
-                }
+        <div className="cart-container">
+          <Cart cart={cart} handleClearCart={handleClearCart}>
+            <Link className="review-order" to="/orders">
+              <button className="cart-delete-btn checkout">
+                Review Order
+                <FontAwesomeIcon
+                  className="cart-delete-icon"
+                  icon={faArrowRight}
+                />
+              </button>
+            </Link>
+          </Cart>
         </div>
-        </>
-    );
+      </div>
+
+      {/* Pagination */}
+      <div className="pagination grid">
+        <div>
+          <p className="text-center">Current Page: {currentPage}</p>
+          <p className="text-center">Items Per Page: {itemsPerPage}</p>
+        </div>
+
+        <div>
+          {pageNumbers.map((number) => (
+            <button
+              key={number}
+              className={currentPage === number ? "selected" : ""}
+              onClick={() => handlePageChange(number)}
+            >
+              {number}
+            </button>
+          ))}
+
+          {/* Drop Down */}
+          <>
+            <select
+              className="border rounded p-2 ml-2 bg-orange-200"
+              value={itemsPerPage}
+              onChange={(e) => {
+                setItemsPerPage(parseInt(e.target.value));
+                setCurrentPage(1); // Reset to the first page when items per page changes
+              }}
+            >
+              {options.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Shop;
